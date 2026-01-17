2b4197a31236df6f9e04092c516ba31dd61b22f7
jonathanzhu <jiasunzhu@gmail.com>
2026-01-17 14:28:18 -0500

    shop

---
+++ frontend/utils/queries.ts
@@ -0,0 +1,33 @@
+export const STOREFRONT_PRODUCTS_QUERY = `query GetProducts {
+  products(first: 10) {
+    edges {
+      node {
+        id
+        title
+        handle
+        description
+        
+        # 1. Get the main featured image
+        featuredImage {
+          id
+          url
+          altText
+        }
+        
+        # 2. Get all media (images, videos, etc.)
+        media(first: 5) {
+          edges {
+            node {
+              ... on MediaImage {
+                image {
+                  url
+                  altText
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
